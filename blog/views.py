03dff67e21339afd0471fce63f7f511090ad28b0
hhx <liuyufei19940309@163.com>
2014-04-06 21:27:25 +0800

    add label

--- blog/views.py
+++ blog/views.py
@@ -19,3 +19,25 @@ def see(request, number):
     if not page:
         raise Http404
     return render_to_response('see.html', {'page': page})
+
+
+def all_label(request):
+    labels = Blog.objects.order_by('label')
+    prev = ''
+    page = []
+    for i in labels:
+        if i.label == prev:
+            continue
+        else:
+            page.append(i.label)
+            prev = i.label
+    if not page:
+        raise Http404
+    return render_to_response('label.html', {'labels': page})
+
+
+def deal_label(request, label_name):
+    blogs = Blog.objects.filter(label__contains=label_name)
+    if not blogs:
+        raise Http404
+    return render_to_response('blog.html', {'blogs': blogs})
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from blog.views import blog, see
+from blog.views import blog, see, all_label, deal_label
 from ustvs.views import us_tvs
 from words.views import words, see_words
 from default.views import home, contact, google, baidu, send_android, send_ios
@@ -23,7 +23,9 @@ urlpatterns = patterns(
     url(r'^google.*$', google),
     url(r'^tc_.*$', baidu),
     url(r'^send_android', send_android),
-	url(r'^send_ios', send_ios),
+    url(r'^send_ios', send_ios),
+    url(r'^all_label', all_label),
+    url(r'^label/(.{1,20})', deal_label),
 )
 
 urlpatterns += patterns(
